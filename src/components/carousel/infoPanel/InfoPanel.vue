092221e43f5274b7d5f2c4ba087a5addb806d4ed
AxlRicci <alexbricci@gmail.com>
2020-05-29 08:13:15 -0400

    created navbar with ability to change infopanel views

--- src/components/carousel/infoPanel/InfoPanel.vue
+++ src/components/carousel/infoPanel/InfoPanel.vue
@@ -1,14 +1,25 @@
 <template>
-  <InfoPanelMain @removeSelected="removeSelected" :itemInfo="itemInfo" />
+  <div class="info-panel--wrapper">
+    <InfoPanelMain
+      v-if="this.view.overview"
+      class="info-panel--view info-panel--view__overview"
+      @removeSelected="removeSelected"
+      :itemInfo="itemInfo"
+    />
+    <InfoPanelNavbar class="info-panel--navbar" @selectedView="selectView" />
+  </div>
+
   <!-- add more panel view components -->
 </template>
 
 <script>
+import InfoPanelNavbar from '@/components/carousel/infoPanel/InfoPanelNavbar'
 import InfoPanelMain from '@/components/carousel/infoPanel/InfoPanelMain'
 
 export default {
   components: {
-    InfoPanelMain
+    InfoPanelMain,
+    InfoPanelNavbar
   },
   props: {
     itemInfo: {
@@ -16,12 +27,57 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      view: {
+        overview: true,
+        episodes: false,
+        related: false,
+        details: false
+      }
+    }
+  },
   methods: {
     removeSelected() {
       this.$emit('removeSelected')
+    },
+    selectView(view) {
+      console.log(view)
+      Object.keys(this.view).forEach(item => {
+        if (view == item) {
+          this.view[item] = true
+        } else {
+          this.view[item] = false
+        }
+      })
     }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.info-panel {
+  &--wrapper {
+    display: grid;
+    grid-template-columns: auto;
+    grid-template-rows: auto auto;
+  }
+
+  &--view {
+    grid-row: 1/3;
+    grid-column: 1/2;
+  }
+
+  &--navbar {
+    grid-row: 2/3;
+    grid-column: 1/2;
+    z-index: 1;
+    background: linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 1) 0%,
+      rgba(0, 0, 0, 0.6446953781512605) 50%,
+      rgba(0, 0, 0, 0) 100%
+    );
+  }
+}
+</style>
---
+++ src/components/carousel/infoPanel/InfoPanelNavbar.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="ip-navbar--wrapper">
+    <ul class="ip-navbar--list">
+      <li
+        class="ip-navbar--item ip-navbar--item__overview"
+        :class="{ selected: selected.overview }"
+        @click="selectView('overview')"
+      >
+        <p><b>OVERVIEW</b></p>
+      </li>
+      <li
+        class="ip-navbar--item ip-navbar--item__episodes"
+        :class="{ selected: selected.episodes }"
+        @click="selectView('episodes')"
+      >
+        <p><b>EPISODES</b></p>
+      </li>
+      <li
+        class="ip-navbar--item ip-navbar--item__related"
+        :class="{ selected: selected.related }"
+        @click="selectView('related')"
+      >
+        <p><b>MORE LIKE THIS</b></p>
+      </li>
+      <li
+        class="ip-navbar--item ip-navbar--item__details"
+        :class="{ selected: selected.details }"
+        @click="selectView('details')"
+      >
+        <p><b>DETAILS</b></p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selected: {
+        overview: true,
+        episodes: false,
+        related: false,
+        details: false
+      }
+    }
+  },
+  methods: {
+    logit(item) {
+      console.log('been clicked', item)
+    },
+    selectView(view) {
+      this.$emit('selectedView', view)
+      Object.keys(this.selected).forEach(item => {
+        if (view == item) {
+          this.selected[item] = true
+        } else {
+          this.selected[item] = false
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ip-navbar {
+  &--list {
+    list-style-type: none;
+    display: flex;
+    justify-content: center;
+  }
+
+  &--item {
+    padding: 20px 30px 0 30px;
+    font-size: 20px;
+    cursor: pointer;
+
+    & p {
+      color: #999;
+      margin: 0;
+    }
+  }
+}
+
+.selected {
+  & p {
+    border-bottom: 5px solid red;
+    padding-bottom: 4px;
+  }
+}
+</style>
