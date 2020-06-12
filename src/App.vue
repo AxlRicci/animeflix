8490edad9aaac36678417f2b6f78699f840978a4
AxlRicci <alexbricci@gmail.com>
2020-06-12 14:50:50 -0400

    added navbar with logo, links, and search (with clickaway view toggle)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div>
+    <div id="app">
+      <div id="nav">
+        <Navbar />
+      </div>
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
 <style>
+* {
+  margin: 0;
+}
 html {
   background: #141414;
 }
@@ -23,6 +36,7 @@ html {
 
 #nav {
   padding: 30px;
+  background: #141414;
 }
 
 #nav a {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="nav--wrapper">
+    <div class="nav--left-wrapper">
+      <div class="nav--logo">
+        <h1>AnimeFlix</h1>
+      </div>
+      <div class="nav--links">
+        <ul class="nav--list">
+          <li class="nav--list-item nav--list-item__home">Anime</li>
+          <li class="nav--list-item nav--list-item__home">Manga</li>
+        </ul>
+      </div>
+    </div>
+    <div class="nav--right-wrapper">
+      <div class="nav--search">
+        <input
+          v-model="search"
+          v-on-clickaway="toggleSearch"
+          v-if="searchToggle"
+          type="text"
+          placeholder="Search"
+        />
+        <img
+          v-if="!searchToggle"
+          @click="toggleSearch"
+          src="@/assets/search.svg"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { directive as onClickaway } from 'vue-clickaway'
+
+export default {
+  directives: {
+    onClickaway: onClickaway
+  },
+  data() {
+    return {
+      search: '',
+      searchToggle: false
+    }
+  },
+  methods: {
+    toggleSearch() {
+      if (!this.search) {
+        this.searchToggle = !this.searchToggle
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  &--wrapper {
+    display: flex;
+    justify-content: space-between;
+    color: #fff;
+  }
+
+  &--left-wrapper {
+    display: flex;
+    align-content: center;
+  }
+
+  &--right-wrapper {
+    display: flex;
+    align-content: center;
+  }
+
+  &--search {
+    align-self: center;
+  }
+  &--logo {
+    & h1 {
+      margin: 0px;
+    }
+  }
+
+  &--links {
+    align-self: center;
+  }
+
+  &--list {
+    display: flex;
+    list-style-type: none;
+    margin: 0;
+
+    &-item {
+      margin-right: 20px;
+    }
+  }
+}
+</style>
